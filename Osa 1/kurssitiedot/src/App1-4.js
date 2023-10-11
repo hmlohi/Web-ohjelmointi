fea99eb24f645ab84b284b981fb1053f340a8ec3
hmlohi <henna.lohi@gmail.com>
2023-10-11 21:53:00 +0300

    Task 1.4 and cleaned 1.3

--- Osa 1/kurssitiedot/src/App1-4.js
+++ Osa 1/kurssitiedot/src/App1-4.js
@@ -1,23 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
- 
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <div>
       <Header course={course} />
-      <Content data1 = {part1} data2 = {part2} data3 = {part3}/>
-      <Total data1 = {part1} data2 = {part2} data3 = {part3}/>
+      <Content data1 = {parts[0]} data2 = {parts[1]} data3 = {parts[2]}/>
+      <Total data1 = {parts[0]} data2 = {parts[1]} data3 = {parts[2]}/>
     </div>
  )
 }
